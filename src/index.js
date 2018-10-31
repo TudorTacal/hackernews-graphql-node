e8ddf60afa71c9d2c9fda4eff0a73a761276905e
Tudor Tacal <t.tacal@elsevier.com>
2018-10-31 19:09:20 +0000

    Create GraphQL server
    - use graphql-yoga
    - next https://www.howtographql.com/graphql-js/2-a-simple-query/

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const { GraphQLServer } = require('graphql-yoga');
+
+const typeDefs = `
+  type Query {
+    info: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    info: () => null,
+  },
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+});
+server.start(() => console.log('Server is running on localhost:4000'));
+
+
