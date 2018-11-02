371b0d8ead21eadc46fe018da7bb7f53c7a75dc3
Tudor Tacal <t.tacal@elsevier.com>
2018-11-02 19:35:51 +0000

    Extend GraphQL API with CRUD functionality for Link

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const resolvers = {
   Query: {
     info: () => 'This is GraphQLs',
     feed: () => links,
+    link: (root, args) => links.find(link => link.id === args.id)
   },
   Mutation: {
     post: (root, args) => {
@@ -21,7 +22,30 @@ const resolvers = {
       }
       links.push(link);
       return link;
-    }
+    },
+    updateLink: (root, args) => {
+      let updatedLink;
+      links.forEach(link => {
+        if (link.id === args.id) {
+          link.description = args.description;
+          link.url = args.url;
+          updatedLink = link;
+        }
+      })
+      return updatedLink;
+    },
+    deleteLink: (root, args) => {
+      let deletedLink;
+      const findLink = link => {
+        if (link.id === args.id) {
+          deletedLink = link;
+          return false;
+        }
+        return true;
+      };
+      links = links.filter(findLink);
+      return deletedLink;
+    },
   },
 };
 
