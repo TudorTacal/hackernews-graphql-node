5acc29a0629c55b9aca154ad793d5c63790ed83a
Tudor Tacal <t.tacal@elsevier.com>
2018-11-02 12:10:59 +0000

    Create a Link query
    i# Please enter the commit message for your changes. Lines starting

--- src/index.js
+++ src/index.js
@@ -3,13 +3,32 @@ const { GraphQLServer } = require('graphql-yoga');
 const typeDefs = `
   type Query {
     info: String!
+    feed: [Link!]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `;
 
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL',
+}];
+
 const resolvers = {
   Query: {
-    info: () => null,
+    info: () => 'This is GraphQLs',
+    feed: () => links,
   },
+  Link: {
+    id: (root) => root.id,
+    description: (root) => root.description,
+    url: (root) => root.url,
+  }
 };
 
 const server = new GraphQLServer({
