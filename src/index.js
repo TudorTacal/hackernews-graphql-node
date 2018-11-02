768dcbdcfbd0c2c9a7b2b40e2104734a2aaf2bd1
Tudor Tacal <t.tacal@elsevier.com>
2018-11-02 12:28:57 +0000

    Create post Mutation for Link

--- src/index.js
+++ src/index.js
@@ -1,38 +1,32 @@
 const { GraphQLServer } = require('graphql-yoga');
 
-const typeDefs = `
-  type Query {
-    info: String!
-    feed: [Link!]!
-  }
-
-  type Link {
-    id: ID!
-    description: String!
-    url: String!
-  }
-`;
-
 let links = [{
   id: 'link-0',
   url: 'www.howtographql.com',
   description: 'Fullstack tutorial for GraphQL',
 }];
+let idCount = links.length;
 
 const resolvers = {
   Query: {
     info: () => 'This is GraphQLs',
     feed: () => links,
   },
-  Link: {
-    id: (root) => root.id,
-    description: (root) => root.description,
-    url: (root) => root.url,
-  }
+  Mutation: {
+    post: (root, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link);
+      return link;
+    }
+  },
 };
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers,
 });
 server.start(() => console.log('Server is running on localhost:4000'));
