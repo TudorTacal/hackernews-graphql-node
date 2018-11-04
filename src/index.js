aee75d8dfc61d1f33bc0d1f9bdba309d91ff33dd
Tudor Tacal <t.tacal@elsevier.com>
2018-11-04 12:15:22 +0000

    Connect server and database with Prisma bindings

--- src/index.js
+++ src/index.js
@@ -1,27 +1,21 @@
 const { GraphQLServer } = require('graphql-yoga');
-
-let links = [{
-  id: 'link-0',
-  url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL',
-}];
-let idCount = links.length;
+const { Prisma } = require('prisma-binding');
 
 const resolvers = {
   Query: {
     info: () => 'This is GraphQLs',
-    feed: () => links,
-    link: (root, args) => links.find(link => link.id === args.id)
+    feed: (root, args, context, info) => {
+      return context.db.query.links({}, info);
+    },
   },
   Mutation: {
-    post: (root, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url,
-      }
-      links.push(link);
-      return link;
+    post: (root, args, context, info) => {
+     return context.db.mutation.createLink({
+       data: {
+         url: args.url,
+         description: args.description,
+       }
+     }, info);
     },
     updateLink: (root, args) => {
       let updatedLink;
@@ -52,6 +46,15 @@ const resolvers = {
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: 'src/generated/prisma.graphql',
+      endpoint: 'https://eu1.prisma.sh/tudortacal-5fcb71/database/dev',
+      secret: 'mysecret123',
+      debug: true,
+    }),
+  })
 });
 server.start(() => console.log('Server is running on localhost:4000'));
 
